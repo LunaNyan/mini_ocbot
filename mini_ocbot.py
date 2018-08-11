69bae7a5af7e98b87224ab3f57d8f5d8ba43f078
ItsLunaNyan <chkong1998@gmail.com>
2018-08-11 13:01:26 +0900

    Add files via upload

---
+++ mini_ocbot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import random
+import re
+
+#Global
+versiontext = "1.0.0"
+
+#mini db
+m_emoticon = ['>_<', '>ㅅ<', '>ㅁ<', 'ㅇㅅㅇ']
+m_emoticon_tail = ['', '~', '~☆', '~//']
+
+t_blush = 
+t_pat = 
+
+
+#Startup
+print ("미니 오너캐봇 v" + versiontext)
+print ("by 루냥이, at 20180811, https://keybase.io/lunanyan")
+print (" ")
+print ("지원 가능한 대화어는 help를 입력하십시오.")
+print (" ")
+
+#main loop
+while 1:
+	inp = input (">>>")
+	if inp = exit:
+		break
+	
