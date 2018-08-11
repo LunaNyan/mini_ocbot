1ccf279bc3b3cb47a677f4af1c49e7fa1418546b
LunaNyan <chkong1998@gmail.com>
2018-08-11 20:05:57 +0900

    멘탈 테스트 2

--- mini_ocbot.py
+++ mini_ocbot.py
@@ -7,18 +7,19 @@ import re
 versiontext = "1.0.0"
 
 #mini db
-m_emoticon = ['>_<', '>ㅅ<', '>ㅁ<', 'ㅇㅅㅇ']
+m_emoticon = ['>_<', '>ㅅ<', '>ㅁ<', '//ㅅ//']
 m_emoticon_tail = ['', '~', '~☆', '~//']
 
 t_blush = ['흐와앗', '후와아', '후에엣', '하와와']
-t_blush_tail = ['..', '~//ㅅ//', '..!']
+t_blush_tail = ['..', '~', '..!']
 
 t_surprised = ['흐엣', '흐왓', '후냣']
 t_surprised_tail = ['..!', '!', '..']
 
 t_pat = ['히냐앙', '후와앗', '헤헤']
 
-
+#Fundamental variables
+pat_firsttime = 0
 
 #Startup
 print ("미니 오너캐봇 v" + versiontext)
@@ -30,6 +31,17 @@ print (" ")
 #main loop
 while 1:
 	inp = input (">>>")
-	if inp = exit:
+	if inp == "exit":
 		break
-	
+	else:
+		a = random.randrange(1,3)
+		if a == 1:
+			if pat_firsttime == 0:
+				print(random.choice(t_surprised)+random.choice(t_surprised_tail))
+				pat_firsttime = 1
+			else:
+				print(random.choice(t_blush)+random.choice(t_blush_tail))
+		elif a == 2:
+			print(random.choice(m_emoticon)+random.choice(m_emoticon_tail))
+		else:
+			print(random.choice(t_pat)+'~')
